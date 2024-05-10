b90235887b13bcd4220dd5ca787bad0e77ce10c4
Siddharth Singh Rana <76247445+ranasiddharth@users.noreply.github.com>
2024-05-11 04:45:54 +0530

    Add files via upload

---
+++ InvertedIndex/src/main/java/org/invertedindex/InvertedIndexJob.java
@@ -0,0 +1,25 @@
+package org.invertedindex;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class InvertedIndexJob {
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "inverted index");
+        job.setJarByClass(InvertedIndexJob.class);
+        job.setMapperClass(InvertedIndexMapper.class);
+        // Commend out this part if you want to use combiner. Mapper and Reducer input and outputs type matching might be needed in this case.
+        //job.setCombinerClass(IntSumReducer.class);
+        job.setReducerClass(InvertedIndexReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(Text.class);
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+}
