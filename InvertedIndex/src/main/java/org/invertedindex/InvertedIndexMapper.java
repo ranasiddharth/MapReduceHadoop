0bf54101e4a0d6828fdfe8d7905e3cf40af0d50d
Siddharth Singh Rana <76247445+ranasiddharth@users.noreply.github.com>
2024-05-11 09:37:04 +0530

    Add files via upload

--- InvertedIndex/src/main/java/org/invertedindex/InvertedIndexMapper.java
+++ InvertedIndex/src/main/java/org/invertedindex/InvertedIndexMapper.java
@@ -6,41 +6,16 @@ import java.util.StringTokenizer;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
-public class InvertedIndexMapper
-        extends Mapper<Object, Text, Text, Text>{
-
-    /*
-    Hadoop supported datatypes. This is a hadoop specific datatype that is used to handle
-    numbers and Strings in a hadoop environment. IntWritable and Text are used instead of
-    Java's Integer and String datatypes.
-    Here 'one' is the number of occurance of the 'word' and is set to value 1 during the
-    Map process.
-    */
-    //private final static IntWritable one = new IntWritable(1);
+public class InvertedIndexMapper extends Mapper<Object, Text, Text, Text>{
     private Text word = new Text();
-
-    public void map(Object key, Text value, Context context
-    ) throws IOException, InterruptedException {
-
-        // Split DocID and the actual text
+    public void map(Object key, Text value, Context context)
+            throws IOException, InterruptedException {
         String DocId = value.toString().substring(0, value.toString().indexOf("\t"));
         String value_raw =  value.toString().substring(value.toString().indexOf("\t") + 1);
-
-        // Reading input one line at a time and tokenizing by using space, "'", and "-" characters as tokenizers.
         StringTokenizer itr = new StringTokenizer(value_raw, " '-");
-
-        // Iterating through all the words available in that line and forming the key/value pair.
         while (itr.hasMoreTokens()) {
-            // Remove special characters
             word.set(itr.nextToken().replaceAll("[^a-zA-Z]", "").toLowerCase());
             if(word.toString() != "" && !word.toString().isEmpty()){
-        /*
-        Sending to output collector(Context) which in-turn passed the output to Reducer.
-        The output is as follows:
-          'word1' 5722018411
-          'word1' 6722018415
-          'word2' 6722018415
-        */
                 context.write(word, new Text(DocId));
             }
         }
--- InvertedIndex/src/main/java/org/invertedindex/InvertedIndexReducer.java
+++ InvertedIndex/src/main/java/org/invertedindex/InvertedIndexReducer.java
@@ -2,24 +2,13 @@ package org.invertedindex;
 
 import java.io.IOException;
 import java.util.HashMap;
-
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
-public class InvertedIndexReducer
-        extends Reducer<Text,Text,Text,Text> {
-    /*
-    Reduce method collects the output of the Mapper calculate and aggregate the word's count.
-    */
-    public void reduce(Text key, Iterable<Text> values,
-                       Context context
+public class InvertedIndexReducer extends Reducer<Text,Text,Text,Text> {
+    public void reduce(Text key, Iterable<Text> values, Context context
     ) throws IOException, InterruptedException {
-
         HashMap<String,Integer> map = new HashMap<String,Integer>();
-      /*
-      Iterable through all the values available with a key [word] and add them together and give the
-      final result as the key and sum of its values along with the DocID.
-      */
         for (Text val : values) {
             if (map.containsKey(val.toString())) {
                 map.put(val.toString(), map.get(val.toString()) + 1);
